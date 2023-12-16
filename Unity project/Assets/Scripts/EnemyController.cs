5ebca77a765272fb3e21d11e59121a8e14ff0afd
Ir1s <145849138+aadharsh99@users.noreply.github.com>
2023-12-16 15:21:47 +0000

    Add files via upload

---
+++ Unity project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private Animator enemyAnim;
+    public int health = 100;
+    public int attackDamage = 10;
+
+    private void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        enemyAnim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // You can add attack logic here based on your game requirements
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Attack()
+    {
+        // Add logic for enemy attacks here
+        // For example, you can deal damage to the player or perform other actions
+    }
+
+    void Die()
+    {
+        // Perform any actions when the enemy dies
+        Destroy(gameObject);
+    }
+}
